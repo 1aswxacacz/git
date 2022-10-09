f21104e2cd44bdf02de7dd11ee0b2c8ac4a2cbf8
cyh <cyh@example.com>
2022-10-09 20:31:48 +0800

    hot-fix commit4

--- src/main/java/com/kuang/Hello.java
+++ src/main/java/com/kuang/Hello.java
@@ -3,11 +3,7 @@ package com.kuang;
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("helloword2");
-        System.out.println("helloword3");
-        System.out.println("helloword3");
-        System.out.println("hotfix test");
-        System.out.println("hotfix test111111111");
+        System.out.println("hotfix test22222222222");
 
     }
 }
